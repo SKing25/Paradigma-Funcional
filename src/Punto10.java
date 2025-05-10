4366c9891c0f0a546c6efe3d5ad3a783846b1399
Sking25 <santiagorodri2509@gmail.com>
2025-05-10 12:41:36 -0500

    correccion 10

--- src/Punto10.java
+++ src/Punto10.java
@@ -12,21 +12,19 @@ public class Punto10 {
             {1, 2, 3, 4, 5},
             {6, 7, 8, 9, 10},
             {11, 12, 13, 14, 15},
-            {16, 17, 18, 19, 20}
-        };
+            {16, 17, 18, 19, 14}};
+
+        System.out.println("Matriz:");
+        Arrays.stream(matriz).forEach(fila -> System.out.println(Arrays.toString(fila)));
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese el número a buscar: ");
         int buscar = scanner.nextInt();
 
-        IntStream.range(0, matriz.length) // Generamos un stream de enteros desde 0 hasta el tamaño de la matriz
+        IntStream.range(0, matriz.length) // Generamos un stream de enteros desde 0 hasta el # de filas de la matriz (4)
                 .forEach(i -> IntStream.range(0, matriz[i].length) // Para cada índice i (fila), crea otro stream de números desde 0 hasta el número de columnas de esa fila
                         .filter(j -> matriz[i][j] == buscar) // Filtramos los índices donde el valor es igual al número buscado
-                        .forEach(j -> System.out.println("El número " + buscar +
-                                " está en la fila " + (i + 1) +
-                                " columna " + (j + 1)))); // Para cada posición encontrada, imprime el número buscado y su posición
-
-        System.out.println("Matriz:");
-        Arrays.stream(matriz).forEach(fila -> System.out.println(Arrays.toString(fila)));
+                        .forEach(j -> System.out.println("El número " + buscar + " está en la fila " + (i + 1) + " columna " + (j + 1))));
+                        // Para cada posición encontrada, imprime el número buscado y su posición
     }
 }
