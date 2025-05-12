acdc5fedadac0f43cb008d4801b416067e025d8a
Sking25 <santiagorodri2509@gmail.com>
2025-05-12 15:37:39 -0500

    correcciones

--- src/Punto5.java
+++ src/Punto5.java
@@ -1,21 +1,17 @@
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.stream.Stream;
 
     /*
         Realizar un algoritmo que calcule la edad de una persona.
     */
 
 public class Punto5 {
+    interface Edad {
+        public int edad(int nacimiento, int hoy);
+    }
+
     public static void main(String[] args) {
-        LocalDate nacimiento = LocalDate.of(2006, 9, 25); // Fecha de nacimiento
-        LocalDate ahora = LocalDate.now(); // Fecha actual
-
-        int edad = Stream.of(nacimiento)
-                .map(fecha -> Period.between(fecha, ahora).getYears()) // Calculamos la edad
-                .findFirst() // Obtenemos el primer elemento del stream
-                .orElse(0); // En caso de que no haya fecha, devolvemos 0
-
-        System.out.println("Edad: " + edad + " años");
+        int nacimiento = 2006;
+        int hoy = 2025;
+        Edad ed = (n, h) -> h - n;
+        System.out.println("La edad es: " + ed.edad(nacimiento, hoy) + " años");
     }
 }
